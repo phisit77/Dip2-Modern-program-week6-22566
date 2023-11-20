27b8e835d6e67c06ba090e885658b639baa1de38
Chatmongcol <cdadwngs@gmail.com>
2023-11-20 15:30:17 +0700

    update

---
+++ bin/week6.dart
@@ -0,0 +1,5 @@
+import 'package:week6/week6.dart' as week6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week6.calculate()}!');
+}
---
+++ test/week6_test.dart
@@ -0,0 +1,8 @@
+import 'package:week6/week6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
